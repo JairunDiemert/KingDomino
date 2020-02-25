f418ea2ab181ec79206bc3bf6902b41bd5b4b4fa
jsd25@students.uwf.edu <jsd25@students.uwf.edu>
2020-02-25 09:11:40 -0600

    Made Tile Class

---
+++ KingDomino/KingDomino/Tile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KingDomino
+{
+	public class Tile
+	{
+		private environmentTypes envType;
+		private int numCrowns;
+		bool filledSpace;
+	}
+}
