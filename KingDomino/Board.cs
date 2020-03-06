40b4c9bd5041414007ae490f90db3a1cf5930d50
jsd25@students.uwf.edu <jsd25@students.uwf.edu>
2020-03-06 16:20:57 -0600

    git ignore

---
+++ KingDomino/Board.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KingDomino
+{
+    public class Board
+    {
+        Tile[, ] Gameboard;     // 8X8
+
+        public Board(){
+            
+            Gameboard = new Tile[8,8];
+            
+            for(int i = 0;i <= 7; ++i){
+                
+                for (int j = 0; j <= 7; ++j){
+                
+                    Gameboard[i,j] = new Tile();
+                }
+            }
+
+        }
+
+        public Tile getTileAt(int row, int col){
+            return Gameboard[row, col];
+        }
+    }
+}
