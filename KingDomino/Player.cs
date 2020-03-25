8cf3769105b84407203c8244275c07bc5dd86637
Mihir Patel <msp20@students.uwf.edu>
2020-03-25 09:59:35 -0500

    Created player class

---
+++ KingDomino/Player.cs
@@ -0,0 +1,25 @@
+using System;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace KingDomino
+{
+	public class Player
+	{
+
+		private Domino[] playerHand; //Array of Dominos available to place
+		private int score; //Track score per player
+		private bool playerStatus; //Determines if player won or lost
+
+		public Player(int score)
+        {
+
+        }
+
+
+	}
+}
