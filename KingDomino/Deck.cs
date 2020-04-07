dd8361770c4039ea053e994e4592adaee7640a8f
jsd25@students.uwf.edu <jsd25@students.uwf.edu>
2020-04-07 14:53:32 -0500

    Refactor of Deck Environment List now using files. Tests for this have been created and passed.

--- KingDomino/Deck.cs
+++ KingDomino/Deck.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Collections;
+using System.IO;
 
 namespace KingDomino
 {
@@ -12,158 +13,15 @@ namespace KingDomino
 
         private ArrayList dominoDeck;
         private int deckSize;
-        private ArrayList envNamesWheatField = new ArrayList
-            {
-                "T2",
-                "T3",
-                "T4",
-                "T5",
-                "T26",
-                "T28",
-                "T30",
-                "T32",
-                "T38",
-                "T40",
-                "T42",
-                "T44",
-                "T46",
-                "T49",
-                "T51",
-                "T53",
-                "T55",
-                "T61",
-                "T63",
-                "T72",
-                "T76",
-                "T81",
-                "T82",
-                "T86",
-                "T91",
-                "T96"
-            };
-        private ArrayList envNamesLakes = new ArrayList
-            {
-                "T14",
-                "T15",
-                "T16",
-                "T17",
-                "T18",
-                "T19",
-                "T29",
-                "T35",
-                "T41",
-                "T57",
-                "T60",
-                "T62",
-                "T64",
-                "T66",
-                "T68",
-                "T70",
-                "T74",
-                "T84"
-            };
-        private ArrayList envNamesMountains = new ArrayList
-            {
-                "T47",
-                "T80",
-                "T90",
-                "T93",
-                "T95",
-                "T97"
-            };
-        private ArrayList envNamesForrest = new ArrayList
-            {
-                "T6",
-                "T7",
-                "T8",
-                "T9",
-                "T10",
-                "T11",
-                "T12",
-                "T13",
-                "T27",
-                "T34",
-                "T36",
-                "T39",
-                "T48",
-                "T50",
-                "T52",
-                "T54",
-                "T56",
-                "T58",
-                "T65",
-                "T67",
-                "T69",
-                "T71"
-            };
-        private ArrayList envNamesVillage = new ArrayList
-            {
-                "T24",
-                "T25",
-                "T33",
-                "T45",
-                "T77",
-                "T79",
-                "T87",
-                "T89",
-                "T92",
-                "T94"
-            };
-        private ArrayList envNamesGarden = new ArrayList
-            {
-                "T20",
-                "T21",
-                "T22",
-                "T23",
-                "T31",
-                "T37",
-                "T43",
-                "T59",
-                "T73",
-                "T75",
-                "T78",
-                "T83",
-                "T85",
-                "T88"
-            };
-        private ArrayList singleCrown = new ArrayList
-            {
-                "T38",
-                "T40",
-                "T42",
-                "T44",
-                "T46",
-                "T48",
-                "T50",
-                "T52",
-                "T54",
-                "T56",
-                "T58",
-                "T60",
-                "T62",
-                "T64",
-                "T66",
-                "T68",
-                "T70",
-                "T73",
-                "T75",
-                "T80"
-            };
-        private ArrayList doubleCrown = new ArrayList
-            {
-                "T83",
-                "T85",
-                "T87",
-                "T89",
-                "T90",
-                "T93",
-                "T95"
-            };
-        private ArrayList tripleCrown = new ArrayList
-            {
-                "T97"
-            };
-
+        private ArrayList envNamesWheatField = CreateArrayListFromFile("envNamesWheatField.txt");
+        private ArrayList envNamesLakes = CreateArrayListFromFile("envNamesLakes.txt");
+        private ArrayList envNamesMountains = CreateArrayListFromFile("envNamesMountains.txt");
+        private ArrayList envNamesForests = CreateArrayListFromFile("envNamesForests.txt");
+        private ArrayList envNamesVillages = CreateArrayListFromFile("envNamesVillages.txt");
+        private ArrayList envNamesGardens = CreateArrayListFromFile("envNamesGardens.txt");
+        private ArrayList singleCrown = CreateArrayListFromFile("envNamesSingleCrown.txt");
+        private ArrayList doubleCrown = CreateArrayListFromFile("envNamesDoubleCrown.txt");
+        private ArrayList tripleCrown = CreateArrayListFromFile("envNamesTripleCrown.txt");
         public Deck()
         {
             DominoDeck = new ArrayList();
@@ -226,12 +84,12 @@ namespace KingDomino
                 if (EnvNamesLakes.Contains(tileImageName2)) envType2 = EnvironmentTypes.Lakes;
                 if (EnvNamesMountains.Contains(tileImageName1)) envType1 = EnvironmentTypes.Mountains;
                 if (EnvNamesMountains.Contains(tileImageName2)) envType2 = EnvironmentTypes.Mountains;
-                if (EnvNamesForrest.Contains(tileImageName1)) envType1 = EnvironmentTypes.Forrest;
-                if (EnvNamesForrest.Contains(tileImageName2)) envType2 = EnvironmentTypes.Forrest;
-                if (EnvNamesVillage.Contains(tileImageName1)) envType1 = EnvironmentTypes.Village;
-                if (EnvNamesVillage.Contains(tileImageName2)) envType2 = EnvironmentTypes.Village;
-                if (EnvNamesGarden.Contains(tileImageName1)) envType1 = EnvironmentTypes.Garden;
-                if (EnvNamesGarden.Contains(tileImageName2)) envType2 = EnvironmentTypes.Garden;
+                if (EnvNamesForests.Contains(tileImageName1)) envType1 = EnvironmentTypes.Forests;
+                if (EnvNamesForests.Contains(tileImageName2)) envType2 = EnvironmentTypes.Forests;
+                if (EnvNamesVillages.Contains(tileImageName1)) envType1 = EnvironmentTypes.Villages;
+                if (EnvNamesVillages.Contains(tileImageName2)) envType2 = EnvironmentTypes.Villages;
+                if (EnvNamesGardens.Contains(tileImageName1)) envType1 = EnvironmentTypes.Gardens;
+                if (EnvNamesGardens.Contains(tileImageName2)) envType2 = EnvironmentTypes.Gardens;
 
                 if (SingleCrown.Contains(tileImageName1)) numCrowns1 = 1;
                 if (SingleCrown.Contains(tileImageName2)) numCrowns2 = 1;
@@ -258,6 +116,22 @@ namespace KingDomino
             }
 
         }
+        public static ArrayList CreateArrayListFromFile(string file)
+        {
+            ArrayList list = new ArrayList();
+            TextReader tr;
+            tr = File.OpenText(file);
+
+            string EnvironmentName;
+            EnvironmentName = tr.ReadLine();
+            while (EnvironmentName != null)
+            {
+                list.Add(EnvironmentName);
+                EnvironmentName = tr.ReadLine();
+            }
+            return list;
+
+        }
 
         public ArrayList DominoDeck
         {
@@ -281,17 +155,17 @@ namespace KingDomino
         {
             get { return envNamesMountains; }
         }
-        public ArrayList EnvNamesForrest
+        public ArrayList EnvNamesForests
         {
-            get { return envNamesForrest; }
+            get { return envNamesForests; }
         }
-        public ArrayList EnvNamesVillage
+        public ArrayList EnvNamesVillages
         {
-            get { return envNamesVillage; }
+            get { return envNamesVillages; }
         }
-        public ArrayList EnvNamesGarden
+        public ArrayList EnvNamesGardens
         {
-            get { return envNamesGarden; }
+            get { return envNamesGardens; }
         }
         public ArrayList SingleCrown
         {
---
+++ KingDominoTests/CreateArrayListFromFileTests.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KingDomino;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KingDomino.Tests
+{
+    [TestClass()]
+    public class CreateArrayListFromFileTests
+    {
+        public void CreateArrayListFromFileTest(string expected, string file, int index)
+        {
+            //Set Up
+            //string expectedField = "T96";
+
+            //Act
+            Deck atTest = new Deck();
+            string actual = (string)Deck.CreateArrayListFromFile(file)[index];
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void Fields()
+        {
+            CreateArrayListFromFileTest("T96", "envNamesWheatField.txt", 25);
+        }
+        [TestMethod()]
+        public void Lakes()
+        {
+            CreateArrayListFromFileTest("T84", "envNamesLakes.txt", 17);
+        }
+        [TestMethod()]
+        public void Mountains()
+        {
+            CreateArrayListFromFileTest("T97", "envNamesMountains.txt", 5);
+        }
+        [TestMethod()]
+        public void Forests()
+        {
+            CreateArrayListFromFileTest("T71", "envNamesForests.txt", 21);
+        }
+        [TestMethod()]
+        public void Villages()
+        {
+            CreateArrayListFromFileTest("T94", "envNamesVillages.txt", 9);
+        }
+        [TestMethod()]
+        public void Gardens()
+        {
+            CreateArrayListFromFileTest("T88", "envNamesGardens.txt",13);
+        }
+        [TestMethod()]
+        public void SingleCrown()
+        {
+            CreateArrayListFromFileTest("T80", "envNamesSingleCrown.txt", 19);
+        }
+        [TestMethod()]
+        public void DoubleCrown()
+        {
+            CreateArrayListFromFileTest("T95", "envNamesDoubleCrown.txt", 6);
+        }
+        [TestMethod()]
+        public void TripleCrown()
+        {
+            CreateArrayListFromFileTest("T97", "envNamesTripleCrown.txt", 0);
+        }
+    }
+}
