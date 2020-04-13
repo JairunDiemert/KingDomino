de4d253003291855b76bea33355f79f7ce4497eb
jsd25@students.uwf.edu <jsd25@students.uwf.edu>
2020-04-13 12:16:24 -0500

    Added "Round" class - will use to work with Deck logic.

---
+++ KingDomino/Round.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KingDomino
+{
+    class Round
+    {
+        public int numberOfPlayers { get; set; }
+        public int playersTurn { get; set; }
+        public bool kingsPlaced { get; set; }
+        public bool tilesPlaced { get; set; }
+        public int totalRounds { get; set; }
+
+        public Round(int numPlayers)
+        {
+            numberOfPlayers = numPlayers;
+            playersTurn = 1;
+            kingsPlaced = false;
+            tilesPlaced = false;
+            totalRounds = 0;
+        }
+    }
+}
