eb02345ce0082501da6813b927915229be85b98a
Leesa Marie Price <literaryleesa@gmail.com>
2020-04-15 22:21:45 -0400

    started round logic class

---
+++ KingDomino/RoundLogic.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KingDomino
+{
+    class RoundLogic
+    {
+        private Player[] players { get; set; }
+        private Player currentPlayerTurn { get; set; }
+
+        public RoundLogic()
+        {
+            players = null;
+            currentPlayerTurn = null;
+        }
+
+        //TODO: Pick domino function w/ meeples
+        //TODO: Place domino function on specific board
+    }
+}
