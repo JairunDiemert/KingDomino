29c78834f6c09bf268afaedffc3ebd435c5b3129
MattMinish <mjm79@students.uwf.edu>
2020-04-17 10:48:45 -0500

    Started movement tests

---
+++ KingDominoTests/MovementTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KingDomino;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KingDominoTests
+{
+    [TestClass()]
+    class MovementTests
+    {
+        [TestMethod()]
+        public void KeyboardMovementTest()
+        {
+            MovementLogic MovementTests = new KingDomino.MovementLogic();
+            
+        }
+
+        [TestMethod()]
+        public void RotationTest()
+        {
+            MovementLogic MovementTests = new KingDomino.MovementLogic();
+        }
+
+        [TestMethod()]
+        public void PlacementTest()
+        {
+            MovementLogic MovementTests = new KingDomino.MovementLogic();
+        }
+    }
+}
