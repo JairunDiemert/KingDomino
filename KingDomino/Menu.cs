af5b58f7959bdbe2bb9bcdd0c460bf6bf7b57aeb
ConnerH50 <crh50@students.uwf.edu>
2020-04-20 20:37:03 -0500

    now when you click play, game will start

--- KingDomino/Menu.cs
+++ KingDomino/Menu.cs
@@ -21,5 +21,13 @@ namespace KingDomino
         {
 
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //using (var game = new Game1())
+            //game.Run();
+            this.Close();
+            
+        }
     }
 }
