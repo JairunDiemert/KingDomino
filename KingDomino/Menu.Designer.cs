934f427ecf140eb0af0977b5ac8ade7b4b577128
jsd25@students.uwf.edu <jsd25@students.uwf.edu>
2020-04-22 14:02:46 -0500

    made background of menu conform to size of window

--- KingDomino/Menu.Designer.cs
+++ KingDomino/Menu.Designer.cs
@@ -41,10 +41,10 @@ namespace KingDomino
             this.button1.Font = new System.Drawing.Font("Bernard MT Condensed", 12F);
             this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.button1.Image = global::KingDomino.Properties.Resources.button;
-            this.button1.Location = new System.Drawing.Point(876, 782);
-            this.button1.Margin = new System.Windows.Forms.Padding(5);
+            this.button1.Location = new System.Drawing.Point(375, 351);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(250, 60);
+            this.button1.Size = new System.Drawing.Size(107, 27);
             this.button1.TabIndex = 0;
             this.button1.Text = "Host";
             this.button1.UseVisualStyleBackColor = true;
@@ -54,10 +54,10 @@ namespace KingDomino
             this.button2.Font = new System.Drawing.Font("Bernard MT Condensed", 12F);
             this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.button2.Image = global::KingDomino.Properties.Resources.button;
-            this.button2.Location = new System.Drawing.Point(876, 696);
-            this.button2.Margin = new System.Windows.Forms.Padding(5);
+            this.button2.Location = new System.Drawing.Point(375, 312);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(250, 60);
+            this.button2.Size = new System.Drawing.Size(107, 27);
             this.button2.TabIndex = 1;
             this.button2.Text = "Join";
             this.button2.UseVisualStyleBackColor = true;
@@ -66,10 +66,10 @@ namespace KingDomino
             // 
             this.button3.Font = new System.Drawing.Font("Bernard MT Condensed", 12F);
             this.button3.Image = global::KingDomino.Properties.Resources.Button_variant2;
-            this.button3.Location = new System.Drawing.Point(876, 610);
-            this.button3.Margin = new System.Windows.Forms.Padding(5);
+            this.button3.Location = new System.Drawing.Point(375, 273);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(250, 67);
+            this.button3.Size = new System.Drawing.Size(107, 30);
             this.button3.TabIndex = 2;
             this.button3.Text = "Play";
             this.button3.UseVisualStyleBackColor = true;
@@ -77,14 +77,16 @@ namespace KingDomino
             // 
             // Menu
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::KingDomino.Properties.Resources.Illustrationg2;
-            this.ClientSize = new System.Drawing.Size(1918, 1009);
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(743, 447);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(5);
+            this.DoubleBuffered = true;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Menu";
             this.Text = "Menu";
             this.Load += new System.EventHandler(this.Menu_Load);
